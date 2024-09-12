6fbaacb07b3a5d88f732b53c76a7f06a60a86ecb
sangkyoung <69530570+bsk2002@users.noreply.github.com>
2024-09-12 17:29:20 +0900

    Title: 최고의 33위치, Time: 6ms, Memory: 0MB, Status: Wrong Answer - CodeTree

--- 240912/최고의 33위치/best-place-of-33.cpp
+++ 240912/최고의 33위치/best-place-of-33.cpp
@@ -7,10 +7,11 @@ int main() {
     cin >> n;
 
     vector<vector<int>> arr(n, vector<int>(n,0));
-
+    int tmp;
     for(int i = 0; i < n; i++){
         for(int j = 0; j < n; j++){
-            cin >> arr[i][j];
+            cin >> tmp;
+            arr[i][j] = tmp;
         }
     }
 

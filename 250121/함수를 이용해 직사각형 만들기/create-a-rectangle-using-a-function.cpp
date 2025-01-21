559a45c369d3c2ac93612bfea498a5f8e9c26517
sangkyoung <69530570+bsk2002@users.noreply.github.com>
2025-01-21 17:40:45 +0900

    Title: 함수를 이용해 직사각형 만들기, Time: 7ms, Memory: 0MB, Status: Passed - CodeTree

--- 250121/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
+++ 250121/함수를 이용해 직사각형 만들기/create-a-rectangle-using-a-function.cpp
@@ -7,7 +7,7 @@ int n, m;
 void print(int n, int m) {
     for(int i = 0; i < n; i++){
         for(int j = 0; j < m; j++){
-            cout << "*";
+            cout << "1";
         }
         cout << endl;
     }

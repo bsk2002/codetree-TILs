61fb155b0d87595e4547109acab38c935158737c
sangkyoung <69530570+bsk2002@users.noreply.github.com>
2025-01-21 17:54:33 +0900

    Title: 최대공약수 구하기, Time: 1052ms, Memory: 0MB, Status: Time Limit Exceed - CodeTree

---
+++ 250121/최대공약수 구하기/find-the-greatest-common-divisor.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+
+using namespace std;
+
+int n, m;
+
+int cal(int n, int m) {
+    int counter = 2;
+    int result = 0;
+    while ( counter > n || counter > m) {
+        if(n % counter == 0 && m % counter == 0) {
+            result = counter;
+        }
+        counter++;
+    }
+
+    return result;
+}
+
+int main() {
+    cin >> n >> m;
+
+    cout << cal(n, m);
+
+    return 0;
+}
